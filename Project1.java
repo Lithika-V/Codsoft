fc1907485828cc99fa921aee075155c0b4fb8f9f
Lithika-V <lithikavias@gmail.com>
2024-10-05 13:59:17 +0530

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,45 @@
+package Codesoft;
+import java.util.*;
+public class Project1 
+{
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean playAgain = true;
+        int totalScore = 0;
+
+        while (playAgain) {
+            int numberToGuess = random.nextInt(100) + 1;
+            int numberOfAttempts = 0;
+            boolean hasGuessedCorrectly = false;
+
+            System.out.println("Guess a number between 1 and 100. You have 7 attempts.");
+
+            while (numberOfAttempts < 7 && !hasGuessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                numberOfAttempts++;
+
+                if (userGuess == numberToGuess) {
+                    hasGuessedCorrectly = true;
+                    System.out.println("Congratulations! You guessed the correct number.");
+                    totalScore += (10 - numberOfAttempts + 1); // Higher score for fewer attempts
+                } else if (userGuess < numberToGuess) {
+                    System.out.println("Too low. Try again.");
+                } else {
+                    System.out.println("Too high. Try again.");
+                }
+            }
+
+            if (!hasGuessedCorrectly) {
+                System.out.println("Sorry, you've used all your attempts. The correct number was " + numberToGuess);
+            }
+
+            System.out.print("Do you want to play another round? (yes/no): ");
+            playAgain = scanner.next().equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Your total score is: " + totalScore);
+        scanner.close();
+    }
+}
